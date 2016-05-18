d8cedbe1aeff37807232dd8cb70cbccaccebe594
Jason <jasona99@outlook.com>
2016-05-18 01:59:18 -0400

    Some more changes to CompareFragment.

--- app/src/main/java/com/ocfc/hardwarecompared/CompareFragment.java
+++ app/src/main/java/com/ocfc/hardwarecompared/CompareFragment.java
@@ -6,8 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -26,6 +31,7 @@ public class CompareFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_compare, container, false);
         setSpinnerContent(view);
+
         return view;
     }
     private void setSpinnerContent( View view )
@@ -38,6 +44,37 @@ public class CompareFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerL.setAdapter(adapter);
         spinnerR.setAdapter(adapter);
+        spinnerL.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        spinnerR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
+
+
 
 }
