8b2e02288deaa0db2fc3f6c55b9fc3cf187f2aad
Jason <jasona99@outlook.com>
2016-05-18 01:24:50 -0400

    Compare fragment updates.

--- app/src/main/java/com/ocfc/hardwarecompared/CompareFragment.java
+++ app/src/main/java/com/ocfc/hardwarecompared/CompareFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -22,8 +24,20 @@ public class CompareFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compare, container, false);
+        View view = inflater.inflate(R.layout.fragment_compare, container, false);
+        setSpinnerContent(view);
+        return view;
+    }
+    private void setSpinnerContent( View view )
+    {
+        Spinner spinnerL = (Spinner) view.findViewById( R.id.spinnerL );
+        Spinner spinnerR = (Spinner) view.findViewById( R.id.spinnerR );
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.gpu_list, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerL.setAdapter(adapter);
+        spinnerR.setAdapter(adapter);
     }
 
 }
