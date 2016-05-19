8fbc04aa30afab59f8a2025d8e17e5893d3c4787
Jason <jasona99@outlook.com>
2016-05-19 03:24:56 -0400

    Small bugfixes and the layout for the build configurator.

--- app/src/main/java/com/ocfc/hardwarecompared/BuildFragment.java
+++ app/src/main/java/com/ocfc/hardwarecompared/BuildFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
@@ -22,8 +24,26 @@ public class BuildFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_build, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_build, container, false);
+        setSpinnerContent(view);
+        return view;
+    }
+    private void setSpinnerContent( View view )
+    {
+        Spinner spinnerRAM = (Spinner) view.findViewById( R.id.spinnerRAM );
+        Spinner spinnerHDD = (Spinner) view.findViewById( R.id.spinnerHDD );
+        Spinner spinnerSSD = (Spinner) view.findViewById( R.id.spinnerSSD );
+        ArrayAdapter<CharSequence> adapterRAM = ArrayAdapter.createFromResource(getContext(),
+                R.array.ram_list, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterStorage = ArrayAdapter.createFromResource(getContext(),
+                R.array.storage_list, android.R.layout.simple_spinner_item);
+            // Specify the layout to use when the list of choices appears
+        adapterRAM.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterStorage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHDD.setAdapter(adapterStorage);
+        spinnerSSD.setAdapter(adapterStorage);
+        spinnerRAM.setAdapter(adapterRAM);
     }
 
 }
