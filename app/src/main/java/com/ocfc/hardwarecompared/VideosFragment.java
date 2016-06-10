10b4b7d8d54259d4f7fd13635fdd4a8b6052d62e
Jason <jasona99@outlook.com>
2016-06-10 06:48:30 -0400

    Video and other bug fixes and changes.

--- app/src/main/java/com/ocfc/hardwarecompared/VideosFragment.java
+++ app/src/main/java/com/ocfc/hardwarecompared/VideosFragment.java
@@ -1,11 +1,16 @@
 package com.ocfc.hardwarecompared;
 
 
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.widget.MediaController;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.VideoView;
 
 
 /**
@@ -22,8 +27,23 @@ public class VideosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false);
+        View view = inflater.inflate(R.layout.fragment_videos, container, false);
+        VideoView videoView;
+        videoView = (VideoView)view.findViewById(R.id.videoView);
+        MediaController mediaController;
+        mediaController = new MediaController(view.getContext());
+        mediaController.setAnchorView(videoView);
+        Uri uri= Uri.parse("rtsp://r3---sn-p5qlsnsr.googlevideo.com/Cj0LENy73wIaNAmJHFypJU6PIhMYDSANFC33lVpXMOCoAUIASARg4eH_zeSxnZZUigELOHpWaFFuM2l2OTQM/78D2C1E485DFFB27AA01BFBF77AD66D3AC546DB8.5269208E21172156935A49A5326441014BC4FDBB/yt6/1/video.3gp");
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+
+
+
+        return view;
     }
+
 
 }
